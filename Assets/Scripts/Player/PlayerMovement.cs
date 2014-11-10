af46108276b411dd2f6dc64e5b91539f5ffbb009
Cecilia Suderman <csmyth7182@gmail.com>
2014-11-09 18:35:35 -0700

    Added variables to PlayerMovement.cs script
    Added various public and private variables to PlayerMovement.cs script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 6f;		// Controls how fast player is
+
+	Vector3 movement;				// Stores movement to be applied to player
+	Animator anim;					// Stores animator applied to player
+	Rigidbody playerRigidbody;		// Stores rigidbody applied to player
+	int floorMask; 					// Used to tell raycast to only hit floor
+	float camRayLength = 100f;		// Length of ray cast from camera
 }
